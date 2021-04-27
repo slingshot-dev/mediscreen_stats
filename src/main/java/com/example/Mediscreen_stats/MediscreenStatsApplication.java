e0a18394821cdb49ebc5bfa37951028b2fb3f7ff
slingshot-dev <slingshot@outlook.fr>
2021-04-27 15:48:56 +0200

    Creation Micro Service mediscreen_stats

---
+++ src/main/java/com/example/Mediscreen_stats/MediscreenStatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mediscreen_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscreenStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscreenStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mediscreen_stats/MediscreenStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mediscreen_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
